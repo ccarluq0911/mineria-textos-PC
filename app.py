c7a6d64bc1bbef9f5802880576db050d354492ec
ccarluq0911 <ccarluq0911@g.educaand.es>
2025-01-05 11:44:03 +0100

    Incluir correctamente campo objetivo

--- app.py
+++ app.py
@@ -17,10 +17,8 @@ def create_model():
   X = pd.read_csv('data/lyrics.csv')
   X = X['Lyrics'].apply(tokenize_text)
   
-  regueton = []
   for i in range(len(X)):
     X[i] = ' '.join(X[i])
-    regueton.append(True)
   
   vectorizer = CountVectorizer()
   X = vectorizer.fit_transform(X)
@@ -28,8 +26,6 @@ def create_model():
   
   vocab = vectorizer.get_feature_names_out()
   print(vocab)
-  
-  X["Regueton"] = regueton
 
   model = MultinomialNB()
   model.fit(X["Lyrics"], X["Regueton"])
--- data/prueba.py
+++ data/prueba.py
@@ -2,6 +2,7 @@ import nltk
 import pandas as pd
 from nltk.tokenize import word_tokenize
 
+'''
 df = pd.read_csv('data.csv')
 df = df['Lyrics']
 df.to_csv('lyrics.csv', index=False)
@@ -10,3 +11,8 @@ nltk.download('punkt_tab')
 
 text = word_tokenize('¿Hola que tal estás?')
 print(text)
+'''
+
+df = pd.read_csv('data/lyrics.csv')
+df['Regueton'] = True
+df.to_csv('data/lyrics.csv', index=False)
