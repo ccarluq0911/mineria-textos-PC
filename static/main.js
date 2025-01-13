7531b141ca91e63d207e816def4714298807a160
PabloE-03 <peibol.max@gmail.com>
2025-01-13 16:30:53 +0100

    Retoques en la parte de front

--- static/main.js
+++ static/main.js
@@ -34,44 +34,57 @@ const comprobar = async ()=>{
 
 const feedback = async(validation)=>{
 
-    const headers = {
-        "Content-Type":"application/json",
-        "validation":validation
-    }
-    let url = "http://localhost:5002/feedback"
-    const texto = document.getElementById("text").value
+    const comprobacion = document.getElementById("info-resultado").textContent
 
-    const response = await fetch(url,{
-        method:"POST",
-        body:JSON.stringify(texto),
-        headers:headers
-    })
-
-    if(response.ok)
+    if(comprobacion!="" && (comprobacion=="Su canción es de reguetón" || comprobacion=="Su canción no es de reguetón"))
     {
-        const tipo_feed = document.createElement("b")
-        tipo_feed.setAttribute("id","color-feed")
-        if(validation)
+        const headers = {
+            "Content-Type":"application/json",
+            "validation":validation
+        }
+        let url = "http://localhost:5002/feedback"
+        const texto = document.getElementById("text").value
+    
+        const response = await fetch(url,{
+            method:"POST",
+            body:JSON.stringify(texto),
+            headers:headers
+        })
+    
+        if(response.ok)
         {
-            const info_feedback = document.getElementById("info-feedback")
-            info_feedback.textContent = "Se ha registrado la letra como "
-            tipo_feed.textContent = "reggaetón"
-            info_feedback.appendChild(tipo_feed)
-            document.getElementById("color-feed").style.color = "forestgreen"
+            const tipo_feed = document.createElement("b")
+            tipo_feed.setAttribute("id","color-feed")
+            if(validation)
+            {
+                const info_feedback = document.getElementById("info-feedback")
+                info_feedback.textContent = "Se ha registrado la letra como "
+                tipo_feed.textContent = "reggaetón"
+                info_feedback.appendChild(tipo_feed)
+                document.getElementById("color-feed").style.color = "forestgreen"
+            }
+            else
+            {
+                const info_feedback = document.getElementById("info-feedback")
+                info_feedback.textContent = "Se ha registrado la letra como "
+                tipo_feed.textContent = "no reggaetón"
+                info_feedback.appendChild(tipo_feed)
+                document.getElementById("color-feed").style.color = "darkred"
+            }
         }
         else
         {
-            const info_feedback = document.getElementById("info-feedback")
-            info_feedback.textContent = "Se ha registrado la letra como "
-            tipo_feed.textContent = "no reggaetón"
-            info_feedback.appendChild(tipo_feed)
-            document.getElementById("color-feed").style.color = "darkred"
+            document.getElementById("info-feedback").textContent = "No se ha podido procesar la solicitud"
+            document.getElementById("info-feedback").style.color = "darkred"
+            document.getElementById("info-feedback").style.fontWeight = "bolder"
         }
     }
     else
     {
-        document.getElementById("info-feedback").textContent = "No se ha podido procesar la solicitud"
+        document.getElementById("info-feedback").textContent = "Antes de validar la canción debes comprobar su género subiendo la letra"
         document.getElementById("info-feedback").style.color = "darkred"
         document.getElementById("info-feedback").style.fontWeight = "bolder"
     }
+
+    
 }
