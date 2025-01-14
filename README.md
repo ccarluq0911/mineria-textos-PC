dda7d65196157f646eca354cf58dbba44c84e6e0
PabloE-03 <peibol.max@gmail.com>
2025-01-14 12:59:18 +0100

    Cambios en el front y en el README

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ python app.py
 ```
 
 Tras esto, se abrirá el servidor en http://localhost:5002
+
+Para hacer portable el proyecto y convertirlo en un .exe ejecutamos
+
+```console
+pyinstaller --onefile --add-data "templates;templates" --add-data "static;static" --add-data "model.pkl;." --add-data "vectorizer.pkl;." app.py
+```
+
+Se creareá una carpeta dist en el proyecto con un ejecutable dentro, ese ejecutable se ha de poner en la raíz del proyecto para que funcione, posteriormente borras la carpeta dist y la carpeta build
--- static/main.js
+++ static/main.js
@@ -1,5 +1,11 @@
 const comprobar = async ()=>{
     const texto = document.getElementById("text").value
+    if(texto.trim()=="")
+    {
+        document.getElementById("info-resultado").textContent = "Debe de colocar una letra antes de subirla a evaluar"
+        document.getElementById("info-resultado").style.color = "darkred"
+        document.getElementById("info-resultado").style.fontWeight = "bolder"
+    }
     let url = "http://localhost:5002/check_genre"
     const response = await fetch(url,{
         method:"POST",
@@ -35,8 +41,8 @@ const comprobar = async ()=>{
 const feedback = async(validation)=>{
 
     const comprobacion = document.getElementById("info-resultado").textContent
-
-    if(comprobacion!="" && (comprobacion=="Su canción es de reguetón" || comprobacion=="Su canción no es de reguetón"))
+    const texto = document.getElementById("text").value
+    if(comprobacion!="" && texto!="" && (comprobacion=="Su canción es de reguetón" || comprobacion=="Su canción no es de reguetón"))
     {
         const headers = {
             "Content-Type":"application/json",
@@ -59,17 +65,21 @@ const feedback = async(validation)=>{
             {
                 const info_feedback = document.getElementById("info-feedback")
                 info_feedback.textContent = "Se ha registrado la letra como "
+                info_feedback.style.color = "black"
                 tipo_feed.textContent = "reggaetón"
                 info_feedback.appendChild(tipo_feed)
                 document.getElementById("color-feed").style.color = "forestgreen"
+                let interval = setInterval(() => cortar(interval), 3000);
             }
             else
             {
                 const info_feedback = document.getElementById("info-feedback")
                 info_feedback.textContent = "Se ha registrado la letra como "
+                info_feedback.style.color = "black"
                 tipo_feed.textContent = "no reggaetón"
                 info_feedback.appendChild(tipo_feed)
                 document.getElementById("color-feed").style.color = "darkred"
+                let interval = setInterval(() => cortar(interval), 3000);
             }
         }
         else
@@ -87,4 +97,15 @@ const feedback = async(validation)=>{
     }
 
     
+}
+
+const cortar = (interval)=>{
+    const tipo_feed = document.createElement("b")
+    tipo_feed.setAttribute("id","color-feed")
+    const info_feedback = document.getElementById("info-feedback")
+    info_feedback.textContent = ""
+    tipo_feed.textContent = ""
+    info_feedback.appendChild(tipo_feed)
+    document.getElementById("color-feed").style.color = "black"
+    clearInterval(interval)
 }
